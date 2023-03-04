d85d7c118a5b2c068eeb18d0b08665b258c898af
Zulu <adekunmbi18@gmail.com>
2023-03-04 06:43:19 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        Pink: "hsl(322, 100%, 66%)",
+        LightPink: "hsl(321, 100%, 78%)",
+        LightRed: "hsl(0, 100%, 63%)",
+        VeryDarkCyan: "hsl(192, 100%, 9%)",
+        VeryPaleBlue: "hsl(207, 100%, 98%)",
+      },
+      fontFamily: {
+        body: ["Public Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
